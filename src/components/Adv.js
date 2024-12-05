42074e8f829a9ba93120486cc61fbd2632c9eac4
shrutiAgarwal08 <shrutigoyal97@gmail.com>
2024-12-05 19:21:34 +0530

    consolidated .gitignore file and deleted unnecessary files

--- src/components/Adv.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-export default function Adv() {
-  return (
-    <>
-     <img src="images\adBand.JPG" className="card-img-top img-fluid" alt="error loading"></img>
-    </>
-  )
-}
